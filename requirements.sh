181e806f4c70c6bcdbcbdffd1954728af50a3dbb
prince-gautam200 <79696153+prince-gautam200@users.noreply.github.com>
2021-08-26 07:19:54 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -2,13 +2,18 @@
 
 echo "THIS SCRIPT IS INSTALLING REQUIRED PREREQUISITES FOR IP-IDENTIFIER"
 echo "--------------------------------------------------------------------------->>>>>>>"
+
 apt install figlet
 apt install curl
 apt install jq
+
 chmod +x ip-identifier
 cp ip-identifier /usr/local/bin/
 
 echo "--------------------------------------------------------------------------->>>>>>>"
+
 echo "IF YOU ARE USING KALI LINUX ------- "
 echo "YOU JUST HAVE TO TYPE 'ip-identifier'"
 echo "IF YOU ARE USING TERMUX ------- "
+echo "YOU JUST HAVE TO TYPE ./ip-identifier"
+
